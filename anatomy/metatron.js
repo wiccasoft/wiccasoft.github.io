77851b7a4ca405656f4fb72d357ebf0c010a29a6
wiccasoft <wiccasoft.xyz@gmail.com>
2026-08-15 00:38:35 +0300

    Update metatron.js

--- anatomy/metatron.js
+++ anatomy/metatron.js
@@ -37,24 +37,23 @@ solfeggio = {core:{u:174,d:285,s:369},middle:{u:417,d:528,s:936},shell:{u:741,d:
 //9*7 green    63 7*9  (7*6 - 7*3 )
 
 
-// colorspectrum dizisi, COLOR_SPECTRUM_MODEL.e verisi metatronu yakma sayısı/sn 
-// 999 - COLOR_SPECTRUM_MODEL.frekans verisiyle ms de söner
+// colorspectrum dizisi, COLOR_SPECTRUM_MODEL.e verisi metatronu yakma sayısı/sn 999 - COLOR_SPECTRUM_MODEL.q verisiyle ms de söner
 
 window.COLOR_SPECTRUM_MODEL = [
 //  777 breath in                                                                         
 // 🔥 YÜKSELEN AKS (Isınma / Genleşme / Sistol): Çarpılarak Katlanır (* 1.618)             777        
-    { id: 1, name: "RED_ENERGY_CHAMBER",    mv: -90, color: "Red",    e: 1.000, frequency: 174, oid: "8" }, // (Phi^0)
-    { id: 2, name: "ORANGE_ABSORB_CHAMBER", mv: -70, color: "Orange", e: 1.618, frequency: 285, oid: "7" }, // (Phi^1)
-    { id: 4, name: "YELLOW_PROPEL_CHAMBER", mv:  20, color: "Yellow", e: 2.618, frequency: 417, oid: "5" }, // (Phi^2)
+    { id: 1, name: "RED_ENERGY_CHAMBER",    mv: -90, color: "Red",    e: 1.000, q: 174, oid: "8" }, // (Phi^0)
+    { id: 2, name: "ORANGE_ABSORB_CHAMBER", mv: -70, color: "Orange", e: 1.618, q: 285, oid: "7" }, // (Phi^1)
+    { id: 4, name: "YELLOW_PROPEL_CHAMBER", mv:  20, color: "Yellow", e: 2.618, q: 417, oid: "5" }, // (Phi^2)
   //                                                                                      -396- 
     // 🤍🖤 THE SACRED HANDS MATRIX (Jesus' Hand Gestures & Orb Placement)                666 fire in the midddle
-    { id: 3, name: "WHITE_LIGHT_CHAMBER",   mv: 100, color: "White",  e: 3.141, frequency: 936, oid: "6"}, // Left hand: blessing the air
-    { id: 6, name: "BLACK_VOID_CHAMBER",    mv: -100,color: "Black",  e: 2.618, frequency: 639, oid: "3"}, // Right hand: holding the earthly orb
+    { id: 3, name: "WHITE_LIGHT_CHAMBER",   mv: 100, color: "White",  e: 3.141, q: 936, oid: "6"}, // Left hand: blessing the air
+    { id: 6, name: "BLACK_VOID_CHAMBER",    mv: -100,color: "Black",  e: 2.618, q: 639, oid: "3"}, // Right hand: holding the earthly orb
 // 222 breath out                                                                         -369-                     
     // 💧 DECAYING AXIS (Contraction / Diastole / Absolute Calm)                           222 
-    { id: 8, name: "GREEN_ENERGY_CHAMBER",  mv:   0, color: "Green",  e: 1.618, frequency: 528, oid: "1" }, // (Phi^2)/(Phi)   
-    { id: 7, name: "BLUE_SHIELD_CHAMBER",   mv: -60, color: "Blue",   e: 1.000, frequency: 741, oid: "2" }, // ((Phi)/(Phi))
-    { id: 5, name: "VIOLET_SHELL_CHAMBER",  mv: -90, color: "Violet", e: 0.618, frequency: 852, oid: "4" }  // fire starter (1.0 / 1.618) + 0.6 
+    { id: 8, name: "GREEN_ENERGY_CHAMBER",  mv:   0, color: "Green",  e: 1.618, q: 528, oid: "1" }, // (Phi^2)/(Phi)   
+    { id: 7, name: "BLUE_SHIELD_CHAMBER",   mv: -60, color: "Blue",   e: 1.000, q: 741, oid: "2" }, // ((Phi)/(Phi))
+    { id: 5, name: "VIOLET_SHELL_CHAMBER",  mv: -90, color: "Violet", e: 0.618, q: 852, oid: "4" }  // fire starter (1.0 / 1.618) + 0.6 
 ];
 
 window.chambers = {core:[174,285,369],cytoplasm:[417,528,936],shell:[741,852,693]}; 
