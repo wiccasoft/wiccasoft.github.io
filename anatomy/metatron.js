033e4cd2c4ff2768c8610da2e4283ed624d136ea
wiccasoft <wiccasoft.xyz@gmail.com>
2026-08-14 20:30:48 +0300

    Update metatron.js

--- anatomy/metatron.js
+++ anatomy/metatron.js
@@ -32,6 +32,7 @@ solfeggio = [174,285,396,417,528,639,741,852,963]; //3,6,9 RGB-(123456789)
 //9*6 orange   54 108 216 432               
 //9*7 green    63 7*9  (7*6 -  7*3 ) 
 
+//colorspectrum dizisi, COLOR_SPECTRUM_MODEL.e verisi ni sn de yakar COLOR_SPECTRUM_MODEL.hz verisiyle ms de söner
 
 window.COLOR_SPECTRUM_MODEL = [
 
