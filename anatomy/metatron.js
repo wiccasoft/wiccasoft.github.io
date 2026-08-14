017934da943da69df7e76675efaa37ea849f3d27
wiccasoft <wiccasoft.xyz@gmail.com>
2026-08-14 23:33:20 +0300

    Update metatron.js

--- anatomy/metatron.js
+++ anatomy/metatron.js
@@ -2,8 +2,8 @@
 // 🪐 METATRON CORE ENGINE - METABOLIC PATHWAY & PURE HEART CORE (432hz)
 // ============================================================================
 
-const M_UP   = [1,4,7]   // 3 
-const M_DOWN = [2,8,5]   // 6 
+const M_UP   = [1,4,7]   // UP 
+const M_DOWN = [2,8,5]   // DOWN
 
 const SPINE = [2,7]
 const METATRON = {RIGHT:[1,2,4],LEFT:[8,7,5],UP:2,BOTTOM:7}
